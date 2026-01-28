125d837f49c2406e0805e1da9db30faf2720f709
DSC-Giboney <giboney@wustl.edu>
2026-01-28 14:17:33 -0600

    CompletedRuin

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -1,35 +1,39 @@
 import java.util.Scanner;
+
 public class Ruin {
-    public static void main(String[] args){
-    
+    public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         System.out.println("Starting amount?");
-        double startAmount, winChance, winLimit;
+        double startAmount, winChance, winLimit, currentAmount;
+        int ruinCount = 0;
+        int totalSimulations = 0;
         startAmount = in.nextDouble();
+        currentAmount = startAmount;
         System.out.println("Win Chance?");
         winChance = in.nextDouble();
         System.out.println("Win Limit?");
         winLimit = in.nextDouble();
-        for (int totalSimulations = 0 ; totalSimulations < 5 ; totalSimulations ++){
-            while(startAmount < winLimit && 0 < startAmount){
-        if (winChance > Math.random()) {
-            startAmount ++;
-            System.out.println("You Win! Your new total is " + startAmount);
-        } else {
-            startAmount --;
-            System.out.println("You Lost! Your new total is " + startAmount);
-        }
-       }
-       if(startAmount >= winLimit){
-        System.out.println("You Win!");
-        startAmount = 0;
-       }else{
-        System.out.println("You Lost!");
-        startAmount = 0;
-       }
+        for (totalSimulations = 0; totalSimulations < 5; totalSimulations++) {
+            while (currentAmount < winLimit && 0 < currentAmount) {
+                if (winChance > Math.random()) {
+                    currentAmount++;
+                    System.out.println("You Win! Your new total is " + currentAmount);
+                } else {
+                    currentAmount--;
+                    System.out.println("You Lost! Your new total is " + currentAmount);
+                }
+            }
+            if (currentAmount >= winLimit) {
+                System.out.println("Day " + (totalSimulations + 1) + ": You Won!");
+            } else {
+                System.out.println("Day " + (totalSimulations + 1) + ": You Lost!");
+                ruinCount ++;
+            }
+            currentAmount = startAmount;
 
         }
+        System.out.println("You ruined " + ((ruinCount * 1.0) / (totalSimulations * 1.0)));
 
-       
     }
 }
